6f49cd7d22de352645b20116b802a6099dc911b5
NikhilaSN <139490864+NikhilaSN@users.noreply.github.com>
2023-07-14 00:08:05 +0530

    Add files via upload

---
+++ Day_2_python.py
@@ -0,0 +1,30 @@
+first name ='nikhila'
+print(frist name)
+input In[3]
+firstname = 'nikhila'
+print(firstname)
+first_name = 'nikhila'
+print(first_name)
+1name ='nikhila'
+print(1name)
+input In[6]
+name1 ='nikhila'
+print(name1)
+name@ = 'nikhila'
+print(name@)
+input In[8]name = 'nikhila' #single quote
+print(name)
+name2 = "pakanati" # double quote
+print(name2)
+type(name)
+str
+type(name2)
+str
+fullname = "nikhila pakanati"
+print(fullname)
+print(fullname.title()) # it willget !st letter is capital
+print(fullname.upper()) # want to fullname in capital letters
+print(fullname.lower()) # want to fullname in smallcase letters
+
+
+
