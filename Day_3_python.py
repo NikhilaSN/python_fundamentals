a1051c0a23cd2cc4989e8b046b2ab78523a27f02
NikhilaSN <139490864+NikhilaSN@users.noreply.github.com>
2023-07-14 00:45:01 +0530

    Add files via upload

---
+++ Day_3_python.py
@@ -0,0 +1,19 @@
+firstname = 'nikki'
+lastname = 'suriya'# want fullname...
+fullname = f"{firstname}{lastname}"
+print(fullname)
+message = f"it is my favourite name,{fullname}"
+print(message)
+message = f"it is my favourite name,{fullname.title()}"
+print("fav_lang:pythonjavacc++swiftjavascript")
+print("fav_lang:\npython\njava\nc\nc++\nswift\njavascript")
+print("fav_lang:\n\tpython\n\tjava\n\tc\n\tc++\n\tswift\n\tjavascript")
+language = ' python'
+print(language)
+language2 = 'python '
+print(language2)
+language3 = ' python '
+print(language3)
+language.lstrip()
+language2.rstrip()
+language3.strip()
